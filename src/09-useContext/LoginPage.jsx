72c120007d0f0d3773cc478426faaccb0aa4d7f9
matisagardia <matisagardia7@gmail.com>
2023-01-13 16:45:28 -0300

    get data from useContext

--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -1,15 +1,29 @@
+import { UserContext } from "./context/UserContext";
+import { useContext } from "react";
 
 
 export const LoginPage = () => {
 
-
+    const {user, setUser} = useContext(UserContext);
 
   return (
     <>
         <h1>LoginPage</h1>
+
         <hr />
     
-    
+        <pre>
+          {JSON.stringify(user, null, 3)}
+        </pre>
+
+        <button className="btn btn-primary"
+                onClick={() => setUser({
+                      id: 123,
+                      name: 'Matias Sagardia',
+                      email: 'matisagardia7@gmail.com'
+                  })}>
+          Set User
+        </button>
     </>
   )
 }
--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,14 +1,15 @@
 import { Link, Outlet } from "react-router-dom";
+import { UserProvider } from "./context/UserProvider";
 import { Navbar } from "./Navbar";
  
 export const MainApp = () => {
  
   return (
-    <>
+    <UserProvider>
         <Navbar />
 
         <hr/>
         <Outlet />
-    </>
+    </UserProvider>
   )
 }
